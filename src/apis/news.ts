c596d1e83b90c9230cea76b0436df14d0082e989
DEV INIKHIYA <57847076+Devichand1@users.noreply.github.com>
2024-08-11 15:08:44 +0530

    logic fixes

--- src/apis/news.ts
+++ src/apis/news.ts
@@ -11,23 +11,14 @@ export const commonHeaders = () => {
   };
 };
 
-export const getNextBatchPage = async (page: number) => {
-  const res = await fetch(END_POINT + page, {
-    method: 'GET',
-    headers: commonHeaders(),
-  });
-  return res.json();
-};
 const fetchData = async ({pageParam}: {pageParam: Number}) => {
   const cached = storage.getString('news');
   if (cached) {
-    console.log('cached data');
     return JSON.parse(cached);
   } else {
     const response = await fetch(`${END_POINT}${pageParam}`, {
       headers: commonHeaders(),
     });
-    console.log('fetched data');
     const data = await response.json();
     storage.set('news', JSON.stringify(data));
     return data;
@@ -47,43 +38,3 @@ export const useNewsApi = () => {
     },
   });
 };
-
-// const fetchJobs = async ({pageParam}: {pageParam: Number}) => {
-//   trackEvent('fetchJobs', {page: pageParam});
-//   const cachedDataKey = `cachedData_page_${pageParam}`;
-//   const cachedTimeKey = `lastFetchedTime_page_${pageParam}`;
-
-//   const cachedData = storage.getString(cachedDataKey);
-//   const lastFetchedAt = storage.getString(cachedTimeKey);
-
-//   const isCacheValid =
-//     lastFetchedAt &&
-//     new Date().getTime() - parseInt(lastFetchedAt, 10) < CACHE_DURATION;
-
-//   if (cachedData && isCacheValid) {
-//     return JSON.parse(cachedData);
-//   } else {
-//     const response = await fetch(`${END_POINT}?page=${pageParam}`);
-//     const data = await response.json();
-//     if (data?.results?.length === 0) {
-//       return null;
-//     }
-//     storage.set(cachedDataKey, JSON.stringify(data));
-//     storage.set(cachedTimeKey, new Date().getTime().toString());
-//     return data;
-//   }
-// };
-
-// export const useHomePageApi = () => {
-//   return useInfiniteQuery({
-//     queryKey: ['jobs'],
-//     queryFn: fetchJobs,
-//     initialPageParam: 1,
-//     getNextPageParam: (lastPage, allPages, lastPageParam) => {
-//       if (!lastPage || lastPage.length === 0) {
-//         return undefined;
-//       }
-//       return lastPageParam + 1;
-//     },
-//   });
-// };
